f0ab54aeb119b2fc2722559de03e357367599282
KonstantinTT <kugliancev@gmail.com>
2025-05-04 12:27:50 +0300

    Add files via upload

--- App.py
+++ App.py
@@ -1,61 +1,68 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+##import urllib.request
+from PIL import Image
     
-    data=pd.read_csv('vehicles_us.csv')
-    import plotly.express as px
+data=pd.read_csv('vehicles_us.csv')
     
-    import streamlit as st
-    
-    st.title('Choose your car')
-    st.subheader('Use this app to find the best car for your wants and needs')
+st.title('Choose your car')
+st.subheader('Use this app to find the best car for your wants and needs')
     
     
-  import urllib.request
-  from PIL import Image
+
     
-  urllib.request.urlretrieve("\\Users\Alex\project_spint6\project_spint6\1036511252.jpg")
-  image = Image.open("1036511252.jpg")
+##urllib.request.urlretrieve('https://www.usnews.com/object/image/0000017c-e20f-d45b-a97d-fbbf2d5f0000/usedcardealership.jpg?update-time=1692736851803&size=responsive640', "usedcardealership.jpg")
+##image = Image.open("usedcardealership.jpg")
+##st.image(image)
     
-  st.Image(img)
-    
-    st.caption(':red[Choose your parameters here]')
+st.caption(':red[Choose your parameters here]')
    
    
-    price_range = st.slider("What is your price range?",
+price_range = st.slider("What is your price range?",
                             value=(1, 375000))
-    actual_range=list(range(price_range[0], price_range[1])+1)
+actual_range=list(range(price_range[0], price_range[1] + 1))
     
     
-    type_range = st.slider("What is your type?",
-                            value=('SUV', 'bus','convertible', 'coupe', 'hatchback', 'mini-van','offroad', 'other', 'pickup', 'sedan', 'truck', 'van','wagon'))
+type_range = st.multiselect("What is your type?",
+                            options=['SUV', 'bus', 'convertible', 'coupe', 'hatchback', 'mini-van', 'offroad', 'other', 'pickup', 'sedan', 'truck', 'van', 'wagon'],
+                            default=['SUV'])
     
     
-  condition_checkbox = st.checkbox("What is your condition?",
-                            value=('excellent', 'fair', 'good', 'like new', 'new', 'poor', 'salvage'))
-    condition_range = st.multiselect("Select the conditions you want to include:", options=condition_checkbox, default=condition_checkbox)
-   
-    st.write("Selected conditions:", condition_range)
-    
+condition_range = st.multiselect("What is your condition?",
+                           options=['excellent', 'fair', 'good', 'like new', 'new', 'poor', 'salvage'],
+                           default=['excellent', 'good'])
+
+st.write("Selected conditions:", condition_range)
+
+fwd_check=st.checkbox('4 wheel drive')
+
+if fwd_check:
+    filtered_data=data[data.is_4wd.isin(actual_range)]
+    filtered_data=filtered_data[filtered_data.is_4wd=='1']
+else:
+    filtered_data=data[data.is_4wd.isin(actual_range)]
     # Filter the data based on user input
-    filtered_data = data[
-        (data['type'].isin(type_range)) &
-        (data['price'].between(price_range[0], price_range[1])) &
-        (data['condition'].isin(condition_range))
-    ]
+filtered_data = data[
+     (data['type'].isin(type_range)) &
+     (data['price'].between(price_range[0], price_range[1])) &
+     (data['condition'].isin(condition_range)&
+      (data['is_4wd'] == 1 if fwd_check else True))]
     
     # Display the filtered data
-    st.write("Filtered Data:", filtered_data)
+st.write("Filtered Data:", filtered_data)
     
     # Create a bar chart of the filtered data
-    fig = px.bar(filtered_data, x='make', y='price', color='condition')
-    st.plotly_chart(fig)
+fig = px.bar(filtered_data, x='price', y='condition')
+st.plotly_chart(fig)
     
-    st.write('Here are your options with a split by price and condition')
+st.write('Here are your options with a split by price and condition')
     
-    fig=px.scatter(filtered_data, x='price', y='condition')
-    sp.plotly_chart(fig2)
+fig2=px.scatter(filtered_data, x='price', y='condition')
+st.plotly_chart(fig2)
     
-    st.write('Here is the list of reccomended cars')
+st.write('Here is the list of reccomended cars')
+try:
     st.dataframe(filtered_data.sample(40))
-    
+except ValueError:
+    st.write('No data available for the selected filters.')
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # project_spint6
+https://github.com/KonstantinTT/project_spint6
+
+https://project-spint6-1.onrender.com
+
